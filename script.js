147d60ca7c06d0187ade2fa3bf629eb4688ad35f
Edwin <edwinjaasiel3@gmail.com>
2025-05-27 20:37:39 -0400

    Primer commit del sitio Deliedvic

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const currentPage = window.location.pathname.split('/').pop();
+  const links = document.querySelectorAll('nav a');
+
+  links.forEach(link => {
+    if(link.getAttribute('href') === currentPage) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+});
